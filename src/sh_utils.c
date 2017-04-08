c10434724d4558a9f235c8de8db441831fed4973
Adam Borowski <kilobyte@angband.pl>
2017-04-08 12:39:01 +0200

    Fix a hang when we're not on a terminal.
    TIOCGWINSZ obviously works only on a tty; otherwise the return value will be
    bogus.  In fact, it can be bogus even on actual terminals -- for example, a
    serial console has no way to send this data and leaves the size as 0x0;
    viewing an image over serial is a quite likely use of catimg (no or clumsy
    ways to send the image over for graphical viewing).  You can simulate such a
    case with "stty columns 0 rows 0".
    
    A more mainstream way to trigger this hang:
        some pipe|while read x;do catimg "$x";done
    which has redirected stdin.
    
    If we're not on a tty, let's use $COLUMNS, it's set by some shells.  If
    that's not set either, 80 is a reasonable default.

--- src/sh_utils.c
+++ src/sh_utils.c
@@ -4,6 +4,8 @@
 #include <windows.h>
 #else
 #include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #endif
 
 uint32_t terminal_columns()
@@ -15,8 +17,21 @@ uint32_t terminal_columns()
         return ret?csbi.dwSize.X:0;
 #else
         struct winsize win;
-        ioctl(0, TIOCGWINSZ, &win);
-        return win.ws_col;
+        const char *colenv;
+
+        if (isatty(0)) {
+                ioctl(0, TIOCGWINSZ, &win);
+                if (win.ws_col > 0)
+                        return win.ws_col;
+        }
+
+        if (colenv = getenv("COLUMNS")) {
+                int c = atoi(colenv);
+                if (c > 0)
+                        return c;
+        }
+
+        return 80;
 #endif
 }
 
